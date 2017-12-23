bafc7b952547894ecfac4b857453959c7f9c3a7f
Unknown <marek.jankech@outlook.com>
2017-12-23 01:50:19 +0100

    At least, beeper and LEDs work

--- main.c
+++ main.c
@@ -5,9 +5,6 @@
  **/
 
 #include <string.h>
-//#include "board.h"
-//#include "pin_mux.h"
-//#include "clock_config.h"
 #include "MK60D10.h"
 
 /*
@@ -46,19 +43,17 @@ void beep(void);
  */
 int main(void) {
 	rtc_time_t time;
-	char recv_str[4];
+	char recv_str[5];
 	char c;
 	int n;
 
-//  BOARD_InitPins();
-//  BOARD_BootClockRUN();
-//  BOARD_InitDebugConsole();
 	MCUInit();
 	PinInit();
 	UART0Init();
-	RTCInit();
+	//RTCInit();
 
   for(;;) {
+	//beep();
 	SendStr("\r\n");
 	SendStr("Prompt");
 	SendStr("\r\n");
@@ -67,6 +62,7 @@ int main(void) {
 	  c = ReceiveCh();
 		  SendCh(c);		// Prijaty znak se hned vysle - echo linky
 		  recv_str[n]=c;		// Postupne se uklada do pole
+		  //beep();
 	}
 	beep();
 	SendStr(strcat("\r\nPoslal si ", recv_str));
@@ -98,17 +94,18 @@ void PinInit(void) {
 	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;  		// Povolit hodiny pre PORTB
 	SIM->SCGC4 |= SIM_SCGC4_UART0_MASK;			// Povolit hodiny pre UART0
 
-	PORTA->PCR[1] = PORT_PCR_MUX(0x02); 		// UART0_RX
-	PORTA->PCR[2] = PORT_PCR_MUX(0x02); 		// UART0_TX
+	PORTA->PCR[1] = (0 | PORT_PCR_MUX(0x02)); 		// UART0_RX
+	PORTA->PCR[2] = (0 | PORT_PCR_MUX(0x02)); 		// UART0_TX
 
 	PORTB->PCR[5] = PORT_PCR_MUX(0x01); 		// MCU_LED0 D9
 	PORTB->PCR[4] = PORT_PCR_MUX(0x01); 		// MCU_LED1 D10
 	PORTB->PCR[3] = PORT_PCR_MUX(0x01); 		// MCU_LED2 D11
 
-	PORTA->PCR[4] = PORT_PCR_MUX(0x01);  		// Beeper (PTA4)
+	PORTA->PCR[4] = (0 | PORT_PCR_MUX(0x01));  		// Beeper (PTA4)
 
-	PTA->PDDR = GPIO_PDDR_PDD(0x0010); 			// PTA4 pin bude vystupny
-	PTB->PDDR = GPIO_PDDR_PDD(0x0034); 			// PTB3/4/5 bude vystupny
+	PTA->PDDR = GPIO_PDDR_PDD(0x0014); 			// PTA1/4 pin bude vystupny
+	PTB->PDDR = GPIO_PDDR_PDD(0x0038); 			// PTB3/4/5 bude vystupny
+	PTB->PDOR |= GPIO_PDOR_PDO(0x0038);			// Zhasnut LED-ky
 }
 
 /*
@@ -121,13 +118,13 @@ void UART0Init(void) {
 	UART0->BDL = 0x1A;						// Baud rate 115 200 Bd
 	UART0->C4 = 0x01;						// Baud rate fine adjust 1/32, match address mode disabled
 	UART0->C1 = 0x00;						// 8 data bitov, bez parity
-	UART0->C2 = (0 | UART_C2_TCIE_MASK);	// transmission complete interrupt enable
-	NVIC_EnableIRQ(UART0_RX_TX_IRQn);		// Enable UART0 receive/transmit interrupt
-	UART0->C3 = 0x00;
-	UART0->MA1 = 0x00;						// no match address (mode disabled in C4)
-	UART0->MA2 = 0x00;						// no match address (mode disabled in C4)
+	//UART0->C2 = (0 | UART_C2_TCIE_MASK);	// transmission complete interrupt enable
+	//NVIC_EnableIRQ(UART0_RX_TX_IRQn);		// Enable UART0 receive/transmit interrupt
+	//UART0->C3 = 0x00;
+	//UART0->MA1 = 0x00;						// no match address (mode disabled in C4)
+	//UART0->MA2 = 0x00;						// no match address (mode disabled in C4)
 	//UART0->S1 |= 0x1F;						// clear IDLE, OR, NF, FE, PF
-	UART0->S2 |= 0xC0;						//clear LBKDIF, RXEDGIF
+	//UART0->S2 |= 0xC0;						//clear LBKDIF, RXEDGIF
 
 	UART0->C2 |= (UART_C2_TE_MASK | UART_C2_RE_MASK);	// Zapnut vysielac a prijimac
 }
@@ -136,8 +133,8 @@ void UART0Init(void) {
  * Inicializacia Real Time Counter
  */
 void RTCInit(void) {
-	//RTC->CR |= RTC_CR_SWR_MASK;				//reset vsetkych RTC registrov
-	RTC->CR = RTC_CR_SWR(0)					//vynulovanie SWR
+	//RTC->CR |= RTC_CR_SWR_MASK;			//reset vsetkych RTC registrov
+	RTC->CR = RTC_CR_SWR(0);				//vynulovanie SWR
 	RTC->TCR = RTC_TCR_CIR(0);				//clear compensation interval
 	RTC->TCR = RTC_TCR_TCR(0);				//clear compensation time
 	RTC->CR |= RTC_CR_OSCE_MASK;			//enable oscillator
@@ -175,7 +172,7 @@ char ReceiveCh(void) {
  */
 void SendStr(char *s)  {
 	int i = 0;
-	while (s[i]!=0)
+	while (s[i] != 0)
 		{SendCh(s[i++]);}
 }
 
